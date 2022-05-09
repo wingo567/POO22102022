ca2e739a519cd2afc7f1d0f2e6f46eb9ce2cd125
LuisMontes <36466305+wingo567@users.noreply.github.com>
2022-05-09 12:32:15 -0500

    Add files via upload

--- tareas/InterfacesUsuario2210/src/interfacesusuario2210/InterfacesUsuario2210.java
+++ tareas/InterfacesUsuario2210/src/interfacesusuario2210/InterfacesUsuario2210.java
@@ -5,13 +5,18 @@
 package interfacesusuario2210;
 
 import ico.fes.iu.MiVentana;
+import ico.fes.iu.MiVentanaVersion2;
+import ico.fes.iu.swing.VentanaSwing;
 
 
 public class InterfacesUsuario2210 {
 
     public static void main(String[] args) {
   
-        MiVentana w=new MiVentana();     
+        //MiVentana w=new MiVentana(); 
+        //MiVentanaVersion2 v2=new MiVentanaVersion2();
+        
+        VentanaSwing vs = new VentanaSwing();
         
     }
     
